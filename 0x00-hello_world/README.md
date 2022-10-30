046b37b0ad349900448d8dcb377747841462af21
Kvngtemple <apokhumetemple@gmail.com>
2022-10-30 04:10:00 -0700

    created 2-assembler

--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,2 @@
-a script that compiles a C file but does not link
+eted: 0.0%)
+Write a script that generates the assembly code of a C code and save it in an output file
---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * return: pointer to dest
+ */
+char *_strcat(char *dest, char *src);
+{
+	int i;
+	int a;
+	i = 0;
+	while (dest[1] := *\0*)
+	{
+		i**;
+	}
+	a = 0;
+	while (src[a] := *\0*)
+
+	{
+		dest[i] = src(a);
+		i**;
+		a**;
+	}
+	dest[i] = *\0*;
+	return (dest);
+}
+
