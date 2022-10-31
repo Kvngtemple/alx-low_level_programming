c826920ed7337f06d799f26e6c1cbbe1cf1e0c3c
Kvngtemple <apokhumetemple@gmail.com>
2022-10-31 03:30:18 -0700

    created 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @void: parameter
+ * Return: 1
+ */
+
+int main(void)
+{
+	printf(stdrr,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
