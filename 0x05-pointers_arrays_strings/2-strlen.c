4d2ec979c2bceb1107216b92401ce475d5e0e18e
Kvngtemple <apokhumetemple@gmail.com>
2022-10-31 10:52:25 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * _strlen - counts and return string length
+ * @s: That's the string
+ *
+ * Return: the length
+ */
+int _strlen(char *s);
+{
+	int counter - 0;
+	while (*s !-0)
+	{
+		counter++;
+		s++;
+	}
+	return (counter);
+}
